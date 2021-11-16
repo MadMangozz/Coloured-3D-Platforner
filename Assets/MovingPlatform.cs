50128551d72c6c9d3d6ad7e16969951cdf1bd49a
Muhammad-Humza Mustafa <mmustafa2021@elam.co.uk>
2021-11-16 12:43:26 +0000

    good

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform myPlatform;
+    public Transform myStartPoint;
+    public Transform myEndPoint;
+    public float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myPlatform.position = myStartPoint.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myPlatform.position = Vector3.MoveTowards(myPlatform.position, myEndPoint.position, speed);
+    }
+}
