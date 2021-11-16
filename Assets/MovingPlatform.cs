466a2a170046cd1d46bb494f9eda35829b14d581
Muhammad-Humza Mustafa <mmustafa2021@elam.co.uk>
2021-11-16 14:37:07 +0000

    gg

--- Assets/MovingPlatform.cs
+++ Assets/MovingPlatform.cs
@@ -8,6 +8,7 @@ public class MovingPlatform : MonoBehaviour
     public Transform myStartPoint;
     public Transform myEndPoint;
     public float speed;
+    bool isReversing = false;
     
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,23 @@ public class MovingPlatform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        myPlatform.position = Vector3.MoveTowards(myPlatform.position, myEndPoint.position, speed);
-    }
+        if (isReversing == false)
+        {
+            myPlatform.position = Vector3.MoveTowards(myPlatform.position, myEndPoint.position, speed);
+            if (myPlatform.position == myEndPoint.position)
+            {
+                isReversing = true;
+            }
+        }
+        else
+        {
+            myPlatform.position = Vector3.MoveTowards(myPlatform.position, myStartPoint.position, speed);
+            if (myPlatform.position == myStartPoint.position)
+            {
+                isReversing = false;
+            }
+        }
+
+
+        }
 }
